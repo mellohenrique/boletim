7098569802fda3370ce461d7a5917192d19192d3
Henrique Assunção <henrique.mello1992@gmail.com>
2021-01-05 10:44:47 -0300

    Corrige falta de pacotes no arquivo de gerar boletim

--- r/gera-boletim.R
+++ r/gera-boletim.R
@@ -1,16 +1,9 @@
 # Carregando pacotes
 ## Esta parte do script funciona para carregar todos os pacotes necessarios para criação do boletim
-
-### Pacotes de terceiros
-use_package <- function(p) {
-  if (!is.element(p, installed.packages()[,1])){
-    install.packages(p, dep = TRUE)}
-  require(p, character.only = TRUE)
-}
-
-use_package("readr")
-use_package("readxl")
-use_package("devtools")
+library(magrittr)
+library(httr)
+library(readr)
+library(devtools)
 
 ### Pacote do boletim
 if (!is.element("codeplan.boletim", installed.packages()[,1])){
@@ -34,6 +27,6 @@ write_excel_csv2(data.table::fread(link, sep = ";", dec = ","), "dados/min_saude
 # Gerando boletim
 gera_boletim(arquivo = "dados/min_saude.csv")
 
-gera_boletim_ra(arquivo = "dados/2020-12-28-cadastro.csv")
+gera_boletim_ra(arquivo = "dados/2021-01-04-cadastro.csv")
 
 # Remova os arquivos baixados posteriormente para não pesar no servidor. 
